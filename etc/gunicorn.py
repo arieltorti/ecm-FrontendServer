4b5729c08ada064f1d60a410c72ed5d62e88f997
Matías Iturburu <maturburu@gmail.com>
2020-06-04 10:44:58 -0300

    Add dev requirements. Add production configs

---
+++ etc/gunicorn.py
@@ -0,0 +1,8 @@
+
+bind = '0.0.0.0:8000'
+workers = 4
+timeout = 30
+error_logfile = '-'
+access_logfile = '/opt/logs/ecm-access.log'
+chdir = '/opt/ecm'
+pythonpath = '/opt/ecm/:/opt/venv/lib/:/opt/venv/lib/python3.8'
