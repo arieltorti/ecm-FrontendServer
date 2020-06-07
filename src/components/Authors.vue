bce77918d3593010c5e95efe4c0e0da716ecf04c
Ariel Torti <arieltorti14@gmail.com>
2020-06-06 23:30:21 -0300

    Add authors

---
+++ src/components/Authors.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <span style="text-decoration: underline">Colaboradores:</span>
+    <p style="margin: 1em 3em">
+      Mauricio Sanfilippo, Francisco Herrero, Ariel Torti, Matias Iturburu,
+      Daniel Gutson, Nicolás Morando (*), Roberto Daniel Rabinovich (*), María de los Ángeles Pando (*)
+    </p>
+    <span
+      >*
+      <a href="http://www.inbirs.org.ar/" style="color: inherit"
+        >Instituto de Investigaciones Biomédicas en Retrovirus y SIDA</a
+      ></span
+    >
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+.container
+    margin: 1.5em 0
+</style>
